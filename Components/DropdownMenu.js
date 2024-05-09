837d0a0197638a255d1841ca80566140bed6005b
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2024-05-09 00:02:38 -0700

    Create DropdownMenu.js

---
+++ Components/DropdownMenu.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import theme from '../theme';
+
+const DropdownMenu = ({ items, selectedItem, setSelectedItem }) => {
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: theme.colors.purple }]}
+        selectedTextStyle={styles.textStyle}
+        itemTextStyle={styles.textStyle}
+        data={items}
+        maxHeight={300}
+        labelField="name"
+        valueField="id"
+        value={selectedItem.id}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={item => {
+          setSelectedItem(item);
+          setIsFocus(false);
+        }}
+      />
+    </View>
+  );
+};
+
+export default DropdownMenu;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.white,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: theme.colors.lightGray,
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 15,
+  },
+  textStyle: {
+    fontSize: 20,
+    fontFamily: theme.fonts.patrickHand,
+    textTransform: 'uppercase',
+  },
+});
