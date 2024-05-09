3318db42fa4aefb126e1ed7e4132403eae608fad
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2024-05-08 21:00:48 -0700

    added placeholder images

--- data/fakeData.js
+++ data/fakeData.js
@@ -1,25 +1,54 @@
 import favicon from "../assets/favicon.png";
+import user1 from "../assets/fakeProfilePhotos/user1.png";
+import user2 from "../assets/fakeProfilePhotos/user2.png";
+import user3 from "../assets/fakeProfilePhotos/user3.png";
+import user4 from "../assets/fakeProfilePhotos/user4.png";
+import user5 from "../assets/fakeProfilePhotos/user5.png";
+import user6 from "../assets/fakeProfilePhotos/user6.png";
+import user7 from "../assets/fakeProfilePhotos/user7.png";
+import user8 from "../assets/fakeProfilePhotos/user8.png";
+import submission1 from "../assets/fakeSubmissionPhotos/submission1.jpeg";
+import submission2 from "../assets/fakeSubmissionPhotos/submission2.jpg";
+import group1 from "../assets/fakeGroupPhotos/group1.jpeg";
 
 export const groups = [
   {
     id: 1,
-    name: "Group 1 this is a name",
-    groupPhoto: favicon,
-    prompt: "This is Group 1",
+    name: "Friends",
+    groupPhoto: group1,
+    ownerId: 1,
+    prompt: "This is the prompt of the day for the Friends group",
     members: [
-      { id: 1, name: "User 1", photo: favicon },
-      { id: 2, name: "User 2", photo: favicon },
-      { id: 3, name: "User 3", photo: favicon },
+      { id: 1, name: "User 1", photo: user1 },
+      { id: 2, name: "User 2", photo: user2 },
+      { id: 3, name: "User 3", photo: user3 },
     ],
     submissions: [
-      { id: 1, photo: favicon, caption: "Caption 1", userId: 1 },
-      { id: 2, photo: favicon, caption: "Caption 2", userId: 2 },
+      { id: 1, photo: submission1, caption: "Caption 1", userId: 1 },
+      { id: 2, photo: submission2, caption: "Caption 2", userId: 2 },
     ]
   },
+  {
+    id: 2,
+    name: "Coworkers",
+    ownerId: 2,
+    prompt: "This is the prompt of the day for the Coworkers group",
+    members: [
+      { id: 1, name: "User 1", photo: user1 },
+      { id: 3, name: "User 3", photo: user3 },
+      { id: 4, name: "User 4", photo: user4 },
+      { id: 5, name: "User 5", photo: user5 },
+      { id: 6, name: "User 6", photo: user6 },
+    ],
+    submissions: [
+      { id: 1, photo: submission1, caption: "Caption 1", userId: 1 },
+      { id: 2, photo: submission2, caption: "Caption 2", userId: 4 },
+    ]
+  }
 ];
 
 export const user = {
   id: 1,
   name: "User 1",
-  photo: favicon,
+  photo: user1,
 };
