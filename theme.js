e0842e1f7c99514104ad7caa537bf0f5cff1f116
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2024-05-09 00:02:18 -0700

    Update theme.js

--- theme.js
+++ theme.js
@@ -10,13 +10,8 @@ const theme = {
       purple: "#7E60FD",
       green: "#00C48C",
     },
-    sizes: {
-      mobile: "767px",
-      tablet: "992px",
-      aboveTablet: "993px",
-      aboveLargeTablet: "1024px",
-      desktop: "1200px",
-      largeDesktop: "1440px"
+    fonts: {
+      patrickHand: "PatrickHand_400Regular"
     }
   };
   
